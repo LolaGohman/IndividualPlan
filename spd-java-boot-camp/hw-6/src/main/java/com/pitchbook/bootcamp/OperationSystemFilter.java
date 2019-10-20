8ac990b5b486fa3fe75dca4089f8524becf9646d
gagavita2014 <lolagohman@gmail.com>
2019-10-20 23:13:03 +0300

    HW-6 Implemented servlet-based app for crawling,  added OS filter

---
+++ spd-java-boot-camp/hw-6/src/main/java/com/pitchbook/bootcamp/OperationSystemFilter.java
@@ -0,0 +1,33 @@
+package com.pitchbook.bootcamp;
+
+import com.pitchbook.bootcamp.exception.OSNotSupportedException;
+import eu.bitwalker.useragentutils.UserAgent;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class OperationSystemFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+       if(isBrowserAllowed(req)){
+           chain.doFilter(req, res);
+       }else{
+           throw new OSNotSupportedException("Your OS does not support this operation!");
+       }
+    }
+
+    private boolean isBrowserAllowed(HttpServletRequest request){
+        UserAgent userAgent = UserAgent.parseUserAgentString(( request)
+                .getHeader("User-Agent"));
+        return !userAgent.getOperatingSystem().getName().equals("WINDOWS_VISTA") ||
+                !userAgent.getOperatingSystem().getName().equals("WINDOWS_XP");
+    }
+
+}
---
+++ spd-java-boot-camp/hw-6/src/main/java/com/pitchbook/bootcamp/exception/OSNotSupportedException.java
@@ -0,0 +1,7 @@
+package com.pitchbook.bootcamp.exception;
+
+public class OSNotSupportedException extends RuntimeException {
+    public OSNotSupportedException(String message){
+        super(message);
+    }
+}
